93af7d28615975e1352bf2a40758ffe3eafbbdd5
PhilipFlood <phili7meath@hotmail.com>
2016-04-22 17:25:29 +0100

    parsing

---
+++ DSS project/src/main/parsetest/jaxb/test.java
@@ -0,0 +1,14 @@
+package jaxb;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("testing");
+
+	}
+
+}
